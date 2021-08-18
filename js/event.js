83490c5721c3d68f577ef5064bdfdf470efb3aaf
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-19 00:23:45 +0600

    event.html updated

---
+++ js/event.js
@@ -0,0 +1,8 @@
+function bodyRed() {
+    document.body.style.backgroundColor = 'red';
+}
+const bodyStyleBlue = document.getElementById('body-blue');
+bodyStyleBlue.onclick = bodyBlue;
+function bodyBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
